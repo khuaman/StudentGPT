44bb880c55b37a9f39d6996354bec7aa145a965c
khuaman <kevin.huaman@utec.edu.pe>
2023-06-10 21:04:39 -0500

    feat: add frontend that fetch data from backend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from psycopg2 import connect, extras
 from os import environ
 from dotenv import load_dotenv
@@ -25,33 +25,94 @@ import openai
 app = Flask(__name__)
 openai.api_key = API_KEY
 conversations = []
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'GET':
-        return render_template('index.html')
-    if request.form['question']:
-        question = 'Yo: ' + request.form['question']
 
-        response = openai.Completion.create(
-            engine = 'text-davinci-003',
-            prompt = question,
-            temperature = 0.5,
-            max_tokens = 150,
-            top_p = 1,
-            frequency_penalty = 0,
-            presence_penalty = 0.6
-        )
 
-        answer = 'AI: ' + response.choices[0].text.strip()
+@app.get('/api/questions')
+def get_questions():
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-        conversations.append(question)
-        conversations.append(answer)
+    cur.execute('SELECT * FROM questions')
+    questions = cur.fetchall()
 
-        return render_template('index.html', chat = conversations)
-    else:
-        return render_template('index.html')
+    cur.close()
+    conn.close()
 
+    return jsonify(questions)
+
+
+@app.post('/api/questions')
+def create_questions():
+    new_question = request.get_json()
+    question = new_question['question']
+    posible_models = ['gpt-4', 'gpt-3.5-turbo', 'text-davinci-003', 'code-davinci-edit-001']
+    model = new_question['model']
+
+    if model not in posible_models:
+        model = posible_models[2]
+
+    response = openai.Completion.create(
+        engine = model,
+        prompt = question,
+        temperature = 0.5,
+        max_tokens = 150,
+        top_p = 1,
+        frequency_penalty = 0,
+        presence_penalty = 0.6
+    )
     
+    answer = response.choices[0].text.strip()
+
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    cur.execute('INSERT INTO questions (question, answer) VALUES (%s, %s) RETURNING *', 
+                (question, answer))
+    new_create_question  = cur.fetchone()
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(new_create_question)
+
+
+@app.delete('/api/questions')
+def delete_question(id):
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    cur.execute('DELETE FROM questions WHERE id = %s RETURNING *', (id))
+    delete_question = cur.fetchone()
+
+    conn.commit()
+    conn.close()
+    cur.close()
+
+    if delete_question is None:
+        return jsonify({'message' : 'Question not found'}), 404
+    
+    return jsonify(delete_question)
+
+
+@app.get('/api/questions/<id>')
+def get_questions_by_id(id):
+    
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+
+    cur.execute('SELECT * FROM questions WHERE id = %s', (id))
+    question = cur.fetchone()
+
+    #Validando por si no existe el registro
+    if question is None:
+        return jsonify({'message' : 'Question not found'}), 404
+    
+    return jsonify(question)
+
+
+@app.route('/')
+def home():
+    return send_file('static/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
---
+++ static/main.js
@@ -0,0 +1,79 @@
+const userForm = document.querySelector('#userForm')
+
+let questions = []
+
+/* Adding an event listener to the `window` object that listens for the `DOMContentLoaded`
+event, which is fired when the initial HTML document has been completely loaded and parsed. When
+this event is triggered, the code sends a GET request to the `/api/questions` endpoint using the
+`fetch` API, and then waits for the response to be returned as JSON data. Once the data is received,
+it is stored in the `questions` array and passed as an argument to the `renderQuestion` function,
+which renders the data on the frontend. */
+
+window.addEventListener('DOMContentLoaded', async ()=>{
+    const response = await fetch('/api/questions');
+    const data = await response.json()
+    questions = data
+    renderQuestion(questions)
+});
+
+/* Adding an event listener to the `userForm` element that listens for the `submit` event.
+When the form is submitted, the function is executed, which prevents the default form submission
+behavior using `e.preventDefault()`. It then retrieves the values of the `question` and `model`
+input fields from the form, and sends a POST request to the `/api/questions` endpoint using the
+`fetch` API. The request includes the `question` and `model` values in the request body as a JSON
+string. Once the response is received, it is converted to JSON format using `response.json()`, and
+the `userForm` is reset to its initial state. */
+
+userForm.addEventListener('submit', async e => {
+    e.preventDefault()
+
+    const question = userForm['question'].value
+    const model = userForm['model'].value
+
+    const response = await fetch('/api/questions',{
+        method : 'POST',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify({
+            question,
+            model
+        })
+    })
+    const data = await response.json()
+
+    questions.unshift(data)
+
+    renderQuestion(questions)
+
+    userForm.reset();
+})
+
+
+/**
+ * The function renders a list of questions with their corresponding models and answers on a web page.
+ * @param questions - an array of objects representing questions, where each object has the following
+ * properties:
+ */
+function renderQuestion(questions){
+    const questionList = document.querySelector('#questionList')
+    questionList.innerHTML = ''
+
+    questions.forEach(element => {
+        const questionItem = document.createElement('li')
+        questionItem.classList = 'list-group-item list-group-item-dark my-2'
+        //<button>edit</button>
+
+        questionItem.innerHTML = `
+        <header class= "d-flex justify-content-between aling-items-center">
+            <h3>${'pregunta: ' + element.question}</h3>
+            <div> 
+                <buttom class= 'bnt-delete btn btn-danger btn-sm'>delete</button>
+            </div>
+        </header>
+        <p>${element.answer}</p>
+        `
+        
+        questionList.append(questionItem)
+    });
+}
